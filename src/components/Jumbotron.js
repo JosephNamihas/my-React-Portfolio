0a58ce77cbd98be0773462e715fe72f9d8098609
Joseph Namihas (Admin) <jnamihasadmin@bsuh.nhs.uk>
2023-03-03 15:00:20 +0000

    Setup Bootstrap. Setup NavBar and Jumbotron

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jumbotron() {
+
+    return (
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <h1 class="display-4">Joseph Namihas</h1>
+      <p class="lead">Welcome to my Web Portfolio</p>
+    </div>
+  </div>
+    )
+}
+
+export default Jumbotron;
